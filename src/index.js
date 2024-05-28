ab5d4e9497bcf96763058cdb9462f992fea91a94
Andre Barros <andrebarros7201@gmail.com>
2024-05-28 21:28:53 +0100

    create files and basic styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Test");
+import "./style.css";
+
+const transform = document.querySelector("#transform");
+const transition = document.querySelector("#transition");
+const keyframes = document.querySelector("#keyframes");
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/index.html"),
-      title: "Test",
+      title: "Advanced CSS",
       filename: "index.html",
     }),
   ],
